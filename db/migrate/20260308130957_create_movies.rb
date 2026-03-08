9ee53a1ee5fd6cc17516d72a1f08d9ce4a14db6f
Tania Kwok <tania@pinkmojito.net>
2026-03-08 17:11:54 +0400

    Movie model created

---
+++ db/migrate/20260308130957_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration[7.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :overview
+      t.integer :rating
+      t.string :poster
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_08_130957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.integer "rating"
+    t.string "poster"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
